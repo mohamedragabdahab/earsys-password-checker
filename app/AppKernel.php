8ecf129c4e9217bd8631401245b6b95f8320bf1a
Mohamed Ragab Dahab <mdahab@treze.co.uk>
2015-07-04 21:42:14 +0200

    Delete unneeded files, and create new bundle: ZanoxAppBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,12 +15,11 @@ class AppKernel extends Kernel {
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Dahab\AppBundle\DahabAppBundle(),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
+            new Zanox\AppBundle\ZanoxAppBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
--- src/Dahab/AppBundle/DahabAppBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Dahab\AppBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class DahabAppBundle extends Bundle
-{
-}
